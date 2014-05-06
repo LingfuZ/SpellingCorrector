a0d463dd259f763daae33a7072d242ab316b3484
Larry114 <lf.zhang86@gmail.com>
2014-05-06 16:00:44 -0600

    Psedo coding Spell and Words

--- src/spell/Spell.java
+++ src/spell/Spell.java
@@ -1,6 +1,9 @@
 package spell;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 /**
  * Created by Lingfu on 5/6/2014.
@@ -8,23 +11,49 @@ import java.io.IOException;
 public class Spell implements SpellCorrector {
 
     // Fields
-//    A data structure to store similar words
+//    A data structure stores similar words
+//    TreeSet is a great option because it doesn't contain duplicates and keep string in alphabetical order
+    Set<String> similarWords = new TreeSet<String>();
+
 
     @Override
     public void useDictionary(String dictionaryFileName) throws IOException {
-
+        /**
+         * Scan the file
+         * Create Words
+         * For each word in dictionary file, add it to Words object
+         *
+         */
     }
 
     @Override
     public String suggestSimilarWord(String inputWord) throws NoSimilarWordFoundException {
 
-
+        /**
+         * Check if input word exists in dictionary file
+         * If yes then return the input word
+         * If no then edit the input word and add them to similar words
+         * Check if there are similar words exists in dictionary
+         * If yes then return "most" similar word
+         * If no then edit each old similar word then add them to new similar words
+         * Check if there are new similar words exists in dictionary
+         * If yes then return "most" similar word
+         * If no then return NoSimilarWordFoundException
+         */
 
         return null;
     }
 
+    private List<String> checkDictionary() {
+        return null;
+    }
+
+    private String findMostSimilarWord(List<String> condidates) {
+        return null;
+    }
+
     public static void main(String[] args) {
-        /*
+        /**
         *   Take first user input as the dictionary file
         *   The second user input is the target word which program will correct its spelling
         *
--- src/spell/Words.java
+++ src/spell/Words.java
@@ -5,33 +5,89 @@ package spell;
  */
 public class Words implements Trie {
 
-    // Fields
+//    Fields
+//    number of unique words stored
+    private int wordCount = -1;
+//    number of word nodes contained
+    private int nodeCount = -1;
+//    root word node
+    private WordNode root = new WordNode();
 
-    @Override
-    public void add(String word) {
+    public Words() {
 
     }
 
     @Override
+    public void add(String word) {
+        /**
+         * Iterate through each character of the word
+         * Check if root has word node for current character
+         * Create a word node if there is none
+         * Get the word node if there is existing one
+         * Increment the node count after create a new node
+         * After add the word, increment the word count
+         */
+    }
+
+    @Override
     public Node find(String word) {
+
+        /**
+         * Similar to add method
+         * Iterate through each character of thw word
+         * Check if has node for current character
+         * Return null if there is none
+         * If last word node returns 0 count then return null
+         * Otherwise return the last word node.
+         */
+
         return null;
     }
 
     @Override
     public int getWordCount() {
-        return 0;
+        return wordCount;
     }
 
     @Override
     public int getNodeCount() {
-        return 0;
+        return nodeCount;
     }
 
     public class WordNode implements Trie.Node {
 
+//      Fields
+//        The size of the alphabet
+        private final int ALPHABET_SIZE = 26;
+//        An array represents alphabet list
+        private WordNode alphabet[] = new WordNode[ALPHABET_SIZE];
+//        count of how many times word appears
+        private int count = -1;
+//        lowercase a for finding the index of characters
+        private final char LOWERCASE_A = 'a';
+
+        public WordNode() {
+
+        }
+
+//        Return a word node in alphabet
+        private WordNode getNode(char character) {
+
+        }
+
+//        Check if there is a word node exist at character index position
+        private boolean hasNode() {
+
+        }
+
+//        Create a new word node at corresponding character index
+        private void createNode(char character) {
+
+        }
+
         @Override
         public int getValue() {
-            return 0;
+            return count;
         }
     }
 }
